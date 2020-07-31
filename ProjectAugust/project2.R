281592de68e548f01bf1d974bceea739db892f4c
Hubar Julien <Julien1941@live.be>
2020-07-31 12:58:08 +0200

    Update project2.R

--- ProjectAugust/project2.R
+++ ProjectAugust/project2.R
@@ -22,6 +22,9 @@ library(MASS)
 library(hexbin)
 library(readtext)
 
+#---------------------------------------------#
+#           Data pre-processing               #
+#---------------------------------------------#
 
 data <- read.table("hcc-data.txt", header = FALSE, na.strings = "?", sep = ",")
 attach(data)
@@ -29,19 +32,31 @@ attach(data)
 #---------------------------------------------#
 #     Strategy to treat missing data          #
 #---------------------------------------------#
-
-
+data <- data%>% select ("V1","V2","V3","V4","V6","V7","V8","V11","V12",
+                        "V13","V14","V15","V16","V17","V19","V20","V21","V22","V23",
+                        "V27","V28","V29","V50","V24","V30","V31","V32","V33",
+                        "V34","V35","V36","V37","V38","V39","V40","V41","V42","V43","V44",
+                        "V45")
 for(i in 1:ncol(data)){
   data[is.na(data[,i]), i] <- mean(data[,i], na.rm = TRUE)
 }
 
+quali_Data <- data %>% select("V1","V2","V3","V4","V6","V7","V8","V11","V12",
+                              "V13","V14","V15","V16","V17","V19","V20","V21","V22","V23",
+                              "V27","V28","V29","V50")
+
+quanti_Data <- data %>% select("V24","V30","V31","V32","V33",
+                               "V34","V35","V36","V37","V38","V39","V40","V41","V42","V43","V44",
+                               "V45")
+
+
 #---------------------------------------------#
 #       Robust outlier detection              #
 #---------------------------------------------#
-quanti_Data <- data %>% select("V24","V25","V26","V30","V31","V32","V33","V34","V35","V36","V37","V38","V39"
-                               ,"V40","V41","V42","V43","V44","V45","V46","V47","V48","V49")
+
 
 ## Use MCD estimator (Coverage parameter of 0.75)
+
 h <- floor((dim(quanti_Data)[1] + dim(quanti_Data)[2] + 1)/2)
 set.seed(0)
 robust <- cov.rob(quanti_Data, cor = TRUE, quantile.used = h, method = "mcd")
