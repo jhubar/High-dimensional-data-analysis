7d7aea3718ba6ca8b38ad3517fe575869aa97a24
Hubar Julien <Julien1941@live.be>
2020-08-12 14:40:55 +0200

    update p3

--- ProjectAugust/3/project3.r
+++ ProjectAugust/3/project3.r
@@ -18,6 +18,8 @@ library(tidyverse)
 library(reshape2)
 library(car)
 library(rgl)
+library(FactoMineR)
+library(factoextra)
 library(MASS)
 library(hexbin)
 library(readtext)
@@ -69,6 +71,8 @@ explanatory_var <- quanti_var
 #---------------------------------------------#
 glm1 <- glm(V50 ~ V24+V30+V31+V32+V33+V34+V35+V36+V37+V38+V39+V40+V41+V42+V43+V44+V45, family = binomial(link = "logit"))
 summary(glm1)
+plot(glm1$linear.predictors,fit1$fitted ,col = colors.th)
+
 #---------------------------------------------#
 #       Draw correlation plot of the          #
 #            explanatory variables            #
@@ -85,84 +89,121 @@ dev.off()
 
 VIF_scores <- vif(glm1)
 print(VIF_scores)
+print(sort(VIF_scores))
+pdf("vif_scores.pdf")
+grPal <- colorRampPalette(c('green','orange','red'))
+Col <- grPal(3)[as.numeric(cut(VIF_scores,breaks = 3))]
+plot(VIF_scores,pch = 20,col=Col)
+abline(h=mean(c(3.791036,2.966739)), col= "red")
+abline(h=mean(c(2.165801,1.702041)), col= "orange")
+dev.off()
 
-# As there is multicolinearity between temp, pres, dewp, try each model by introducing one of them
-# And compare the respective coefficients to the ones obtained in the full model.
-# In full model: temp ~ -0.24125; pres ~ -0.07032, dewp ~ 0.15057
-# In respective models: temp ~ -0.023704, pres ~ -0.0001157, dewp ~ -0.0005504
-glm_temp <- glm(alert ~ year + month + temp + rain + wspd + wdir, family = binomial(link = "logit"))
-summary(glm_temp)
+#---------------------------------------------#
+#       Compute glmV38,39,40,41               #
+#---------------------------------------------#
+glmV38 <- glm(V50 ~ V24+V30+V31+V32+V33+V34+V35+V36+V37+V38+V42+V43+V44+V45, family = binomial(link = "logit"))
+summary(glmV38)
+glmV39 <- glm(V50 ~ V24+V30+V31+V32+V33+V34+V35+V36+V37+V39+V42+V43+V44+V45, family = binomial(link = "logit"))
+summary(glmV39)
+glmV40 <- glm(V50 ~ V24+V30+V31+V32+V33+V34+V35+V36+V37+V40+V42+V43+V44+V45, family = binomial(link = "logit"))
+summary(glmV40)
+glmV41 <- glm(V50 ~ V24+V30+V31+V32+V33+V34+V35+V36+V37+V41+V42+V43+V44+V45, family = binomial(link = "logit"))
+summary(glmV41)
+#---------------------------------------------#
+#       PCA(V38,39,40,41) -> model            #
+#---------------------------------------------#
+pca <- princomp(data[c('V38', 'V39', 'V40','V41')])
+fviz_pca_var(res.pca, col.var = "black")
+res.pca <- prcomp(data[c('V38', 'V39', 'V40','V41')], scale = TRUE)
+fviz_eig(res.pca)
+fviz_pca_var(res.pca,
+             col.var = "contrib", 
+             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+             repel = TRUE     
+)
+explanatory_var_updated <- c('V24','V30','V31','V32','V33','V34','V35','V36','V37','V42','V43','V44','V45', 'PC1')
+data_Updated <- data[c('V24','V30','V31','V32','V33','V34','V35','V36','V37','V42','V43','V44','V45','V50')]
+data_Updated['PC1'] <- pca$scores[, 1]
+attach(data_Updated)
 
-glm_pres <- glm(alert ~ year + month + pres + rain + wspd + wdir, family = binomial(link = "logit"))
-summary(glm_pres)
-
-glm_dewp <- glm(alert ~ year + month + dewp + rain + wspd + wdir, family = binomial(link = "logit"))
-summary(glm_dewp)
-
-# As multicolinearity introduces an issue here, build model with PC1 of pres, temp, dewp and with remaining variables
-pca <- princomp(data[c('temp', 'pres', 'dewp')])
-modified_explanatory_var <- c('year', 'month', 'rain', 'wspd', 'wdir', 'PC1')
-
-modified_data <- data[c('year', 'month', 'rain', 'wspd', 'wdir', 'alert')]
-modified_data['PC1'] <- pca$scores[, 1]
-attach(modified_data)
-
-linear_classification <- glm(alert ~ year + month + rain + wspd + wdir + PC1, family = binomial(link = "logit"))
-summary(linear_classification)
+glm2 <- glm(V50 ~ V24+V30+V31+V32+V33+V34+V35+V36+V37+V42+V43+V44+V45+PC1, family = binomial(link = "logit"))
+summary(glm2)
 
 # Conduct variable selection using backward selection.
-stepAIC(linear_classification)
-
+stepAIC(glm2)
+plot(glmSuper)
+dev.off()
 # Conduct variable selection using forward selection.
-null_model <- glm(alert ~ 1, family = binomial(link = "logit"))
-stepAIC(null_model, scope = alert ~ year + month + PC1 + rain + wspd + wdir, direction = "forward")
+model_null <- glm(V50 ~ 1, family = binomial(link = "logit"))
+stepAIC(model_null, scope = V50 ~ V24+V30+V31+V32+V33+V34+V35+V36+V37+V42+V43+V44+V45+PC1, direction = "forward")
 
-# Remove rain from model
-modified_model <- glm(alert ~ year + month + wspd + wdir + PC1, family = binomial(link = "logit"))
-modified_explanatory_var <- c('year', 'month', 'wspd', 'wdir', 'PC1')
+# Remove V30, V31, V33, V34, V35, V42, V43, V44 from model
+model_updated <- glm(V50 ~ V24 + V32 + V36 + V37 + V45 + PC1,family = binomial(link = "logit"))
+explanatory_var_updated <- c('V24', 'V32', 'V36', 'V37', 'V45','PC1')
 
-# Predict
-predictions <- (predict.glm(object = modified_model, newdata = modified_data[, modified_explanatory_var], type = "response") > 0.5)
-errors <- (modified_data['alert'] != predictions) # /!\ error on the TRAINING set ()
+res.pca_model_final <- prcomp(data_Updated[c('V24', 'V32', 'V36', 'V37', 'V45','PC1')], scale = TRUE)
+fviz_eig(res.pca_model_final)
+pdf("circle_pca.pdf")
+fviz_pca_var(res.pca_model_final,
+             col.var = "contrib", 
+             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+             repel = TRUE     
+)
+dev.off()
+
+
+#----------------------------------------#
+#                 Predict                #
+#----------------------------------------#
+predictions <- (predict.glm(object = model_updated, newdata = data_Updated[, explanatory_var_updated], type = "response") > 0.5)
+errors <- (data_Updated['V50'] != predictions) 
 mean(errors)
-
-# Look at fitted values
-fitted_values <- modified_model$fitted.values
+#----------------------------------------#
+#              fitted values             #
+#----------------------------------------#
+fitted_values <- model_updated$fitted.values
 summary(fitted_values)
 plt <- ggplot(data.frame(fitted_values), aes(y = fitted_values)) + geom_boxplot()
 plt <- plt + labs(x = "", y = "Fitted values")
 ggsave("boxplots_fitted_values.pdf", plt)
 
-# Look at Pearson residuals
-residuals <- resid(modified_model, type = "pearson")
+#----------------------------------------#
+#     Look at Pearson residuals          #
+#----------------------------------------#
+residuals <- resid(model_updated, type = "pearson")
 summary(residuals)
 plt <- ggplot(data.frame(residuals), aes(y = residuals)) + geom_boxplot()
 plt <- plt + labs(x = "", y = "Pearson residuals")
 ggsave("boxplots_residuals.pdf", plt)
 
-# Part 2 : Leave-One-Out cross-validation
-# Use model with PC1 and without rain
-LOO_Posterior <- rep(0, dim(modified_data)[1])
-for (i in 1:dim(modified_data)[1]) {
-  idx <- rep(TRUE, dim(modified_data)[1])
+#----------------------------------------#
+#     CV with model_Updated              #
+#----------------------------------------#
+
+LOO_Posterior <- rep(0, dim(data_Updated)[1])
+for (i in 1:dim(data_Updated)[1]) {
+  idx <- rep(TRUE, dim(data_Updated)[1])
   idx[i] <- FALSE
-  
-  linear_classification <- glm(alert[idx] ~ year[idx] + month[idx] + PC1[idx] + wspd[idx] + wdir[idx], family = binomial(link = "logit"))
-  prediction <- linear_classification$coefficients %*% c(1, as.matrix(modified_data[i, modified_explanatory_var]))
+  linear_classification <- glm(V50[idx] ~ V24[idx] + V32[idx] + V36[idx] + V37[idx] + V45[idx] + PC1[idx], family = binomial(link = "logit"))
+  prediction <- linear_classification$coefficients %*% c(1, as.matrix(data_Updated[i, explanatory_var_updated]))
   LOO_Posterior[i] <- exp(prediction)/(1 + exp(prediction))
 }
 
 # Construct confusion matrix
-conf_mat <- table(alert, as.integer(LOO_Posterior > 0.5))
+Actual<- V50
+Predicted <- as.integer(LOO_Posterior > 0.5)
+#conf_mat <- table(Predicted,Actual)
+conf_mat <- table(Predicted,Actual)
+print(conf_mat)
 
-# Compute sensitivity and specificity
+#  sensitivity and specificity
 sens_or_spec <- function(confusion_matrix, spec = FALSE) {
   if (spec) {
-    specificity <- (confusion_matrix[1,1])/(confusion_matrix[1,1] + confusion_matrix[1,2])
+    specificity <-(confusion_matrix[2,2])/(confusion_matrix[2,2] + confusion_matrix[1,2])
     return(specificity)
   }
   else {
-    sensitivity <- (confusion_matrix[2,2])/(confusion_matrix[2,2] + confusion_matrix[2,1])
+    sensitivity <-(confusion_matrix[1,1])/(confusion_matrix[1,1] + confusion_matrix[2,1])
     return(sensitivity)
   }
 }
@@ -181,31 +222,33 @@ ROC_maker <- function(scores, memberships) {
     data <- as.data.frame(scores)
     data["Membership"] <- as.integer(data > cut)
     
-    confusion_matrix <- table(memberships, data[, "Membership"])
+    confusion_matrix <- table( data[, "Membership"],memberships)
     sensitivity[idx] <- sens_or_spec(confusion_matrix)
     specificity[idx] <- sens_or_spec(confusion_matrix, spec = TRUE)
     idx <- idx + 1
   }
-  sensitivity <- c(sensitivity, 0)
-  specificity <- c(specificity, 1)
+  sensitivity <- c(sensitivity, 1)
+  specificity <- c(specificity, 0)
   plot(1 - specificity, sensitivity, type="l")
   
   return(list(spec = (1 - specificity), sens = sensitivity))
 }
 
-ROC <- ROC_maker(LOO_Posterior, alert)
+pdf("ROC.pdf")
+ROC <- ROC_maker(LOO_Posterior, V50)
 abline(a = 0, b = 1, col = "red")
-
+dev.off()
 # Compute AUC
 AUC <- function(ROCx, ROCy)
 {
   n <- length(ROCx)
-  base <- ROCx[1:(n-1)] - ROCx[2:n]
+  base <-   ROCx[2:n]-ROCx[1:(n-1)]
   height <- ROCy[2:n]
   return(sum(base*height))
 }
 auc <- AUC(ROCx = ROC$spec, ROCy = ROC$sens)
 print(auc)
+
 #-----------------------------------------------------# 
 #           Linear Discriminant Analysis              #
 #-----------------------------------------------------# 
