3c63d820742f7b98c1042f6447f54448f74f3d07
Hubar Julien <Julien1941@live.be>
2020-08-27 17:26:43 +0200

    update

--- ProjectAugust/3/project3.r
+++ ProjectAugust/3/project3.r
@@ -37,7 +37,7 @@ attach(data)
 data <- data%>% dplyr::select ("V1","V2","V3","V4","V6","V7","V8","V11","V12",
                         "V13","V14","V15","V16","V17","V19","V20","V21","V22","V23",
                         "V27","V28","V29","V50","V24","V30","V31","V32","V33",
-                        "V34","V35","V36","V37","V38","V39","V40","V41","V42","V43","V44",
+                        "V34","V35","V36","V37","V38","V40","V41","V42","V43","V44",
                         "V45")
 quali_Data <- data %>% dplyr::select("V1","V2","V3","V4","V6","V7","V8","V11","V12",
                               "V13","V14","V15","V16","V17","V19","V20","V21","V22","V23",
@@ -45,7 +45,7 @@ quali_Data <- data %>% dplyr::select("V1","V2","V3","V4","V6","V7","V8","V11","V
 for(i in 1:ncol(data)){
   data[is.na(data[,i]), i] <- mean(data[,i], na.rm = TRUE)
 }
-
+attach(data)
 
 #dataM <- data[V1=='1',]
 #quantDataM <- dataM %>% dplyr::select("V24","V30","V31","V32","V33","V34","V35","V36","V37","V38","V39","V40","V41","V42","V43","V44","V45")
@@ -68,7 +68,7 @@ quantData <- data %>% dplyr::select("V24","V30","V31","V32","V33",
 #---------------------------------------------#
 #               Complete model                #
 #---------------------------------------------#
-test <- glm(data$V50~., family = binomial(link = "logit"), data = data)
+test <- glm(V50~., family = binomial(link = "logit"), data = data)
 summary(test)
 stepAIC(test)
 
@@ -98,61 +98,86 @@ pdf("correlation_plot_of_explanatory_variables.pdf")
 corrplot(corrplot_of_explanatory_variables, method = "circle", type = "lower", tl.col = "black", tl.pos = "ld", tl.srt = 45)
 dev.off()
 
-attach(data)
+detach(data)
 #---------------------------------------------#
 #       Compute glmV38,39,V20                 #
 #---------------------------------------------#
+attach(dataV20)
+dataV20 <- data%>% dplyr::select ("V1","V2","V3","V4","V6","V7","V8","V11","V12",
+                               "V13","V14","V15","V16","V17","V19","V20","V21","V22","V23",
+                               "V27","V28","V29","V50","V24","V30","V31","V32","V33",
+                               "V34","V35","V36","V37","V40","V41","V42","V43","V44",
+                               "V45")
 
-glmV20 <- glm(data$V50 ~ V1+V2+V3+V4+V6+V7+V8+V11+V12V+13+V14+V15+V16+V17+V19+V20+V21+V22+V23+V27+V28+V29+V24+V30+V31+V32+V33+V34+V35+V36+V37+V40+V41+V42+V43+V44+V45,
-              family = binomial(link = "logit"),data = data)
+glmV20 <- glm(V50 ~.,family = binomial(link = "logit"), data = dataV20)
 summary(glmV20)
-glmV38 <- glm(V50 ~ V1+V2+V3+V4+V6+V7+V8+V11+V12V+13+V14+V15+V16+V17+V19+V21+V22+V23+V27+V28+V29+
-                V24+V30+V31+V32+V33+V34+V35+V36+V37+V38+V40+V41+V42+V43+V44+V45, family = binomial(link = "logit"), data = data)
-summary(glmV38)
-dataV38 <- data%>% dplyr::select(data,-starts_with("V20","V39"))
-glmV39 <- glm(V50 ~ V1+V2+V3+V4+V6+V7+V8+V11+V12V+13+V14+V15+V16+V17+V19+V21+V22+V23+V27+V28+V29+
-                V24+V30+V31+V32+V33+V34+V35+V36+V37+V38+V39+V40+V41+V42+V43+V44+V45, family = binomial(link = "logit"))
-summary(glmV39)
+detach(dataV20)
 
+dataV38 <- data%>% dplyr::select ("V1","V2","V3","V4","V6","V7","V8","V11","V12",
+                                  "V13","V14","V15","V16","V17","V19","V21","V22","V23",
+                                  "V27","V28","V29","V50","V24","V30","V31","V32","V33",
+                                  "V34","V35","V36","V37","V38","V40","V41","V42","V43","V44",
+                                  "V45")
+attach(dataV38)
+glmV38 <- glm(V50 ~.,family = binomial(link = "logit"), data = dataV38)
+summary(glmV38)
+
+
+detach(dataV38)
+dataV39 <- data%>% dplyr::select ("V1","V2","V3","V4","V6","V7","V8","V11","V12",
+                                  "V13","V14","V15","V16","V17","V19","V20","V21","V22","V23",
+                                  "V27","V28","V29","V50","V24","V30","V31","V32","V33",
+                                  "V34","V35","V36","V37","V39","V40","V41","V42","V43","V44",
+                                  "V45")
+attach(dataV39)
+glmV39 <- glm(V50 ~.,family = binomial(link = "logit"), data = dataV39)
+summary(glmV39)
+detach(dataV39)
+attach(data)
 summary(test)
 #---------------------------------------------#
-#       PCA(V38,39,40,41) -> model            #
+#       PCA(V38,39) -> model            #
 #---------------------------------------------#
-pca <- princomp(data[c('V38', 'V39', 'V40','V41')])
-fviz_pca_var(res.pca, col.var = "black")
-res.pca <- prcomp(data[c('V38', 'V39', 'V40','V41')], scale = TRUE)
-fviz_eig(res.pca)
-fviz_pca_var(res.pca,
+pca <- princomp(data[c('V20','V38', 'V39')])
+fviz_pca_var(pca, col.var = "black")
+pca <- prcomp(data[c('V38', 'V39')], scale = TRUE)
+fviz_eig(pca)
+fviz_pca_var(pca,
              col.var = "contrib", 
              gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
              repel = TRUE     
 )
-explanatory_var_updated <- c('V24','V30','V31','V32','V33','V34','V35','V36','V37','V42','V43','V44','V45', 'PC1')
-data_Updated <- data[c('V24','V30','V31','V32','V33','V34','V35','V36','V37','V42','V43','V44','V45','V50')]
+
+explanatory_var_updated <- c('V1','V2','V3','V4','V6','V7','V8','V11','V12','V13','V14','V15','V16','V17','V19','V20','V21','V22','V23','V27','V28','V29','V50','V24','V30','V31','V32','V33','V34','V35','V36','V37','V40','V41','V42','V43','V44','V45', 'PC1')
+data_Updated <- data[c('V1','V2','V3','V4','V6','V7','V8','V11','V12','V13','V14','V15','V16','V17','V19','V20','V21','V22','V23','V27','V28','V29','V50','V24','V30','V31','V32','V33','V34','V35','V36','V37','V40','V41','V42','V43','V44','V45','V50')]
 data_Updated['PC1'] <- pca$scores[, 1]
 
-attach(data_Updated)
-for(i in 1:ncol(data_Updated)){
-  data_Updated[is.na(data_Updated[,i]), i] <- mean(data_Updated[,i], na.rm = TRUE)
-}
-glm2 <- glm(V50 ~ V24+V30+V31+V32+V33+V34+V35+V36+V37+V42+V43+V44+V45+PC1, family = binomial(link = "logit"))
+
+detach(data)
+
+data <- data%>% dplyr::select ("V1","V2","V3","V4","V6","V7","V8","V11","V12",
+                                  "V13","V14","V15","V16","V17","V19","V20","V21","V22","V23",
+                                  "V27","V28","V29","V50","V24","V30","V31","V32","V33",
+                                  "V34","V35","V36","V37","V38","V40","V41","V42","V43","V44",
+                                  "V45")
+attach(data)
+glm2 <- glm(V50~V1+V2+V3+V4+V6+V7+V8+V11+V12+V13+V14+V15+V16+V17+V19+V20+V21+V22+V23+V27+V28+V29+V24+V30+V31+V32+V33+V34+V35+V36+V37+V38+V40+V41+V42+V43+V44+V45, family = binomial(link = "logit"), data = data)
 summary(glm2)
 
 
 
-# Conduct variable selection using backward selection.
+# backward selection.
 stepAIC(glm2)
-plot(glmSuper)
-dev.off()
-# Conduct variable selection using forward selection.
+
+# forward selection.
 model_null <- glm(V50 ~ 1, family = binomial(link = "logit"))
-stepAIC(model_null, scope = V50 ~ V24+V30+V31+V32+V33+V34+V35+V36+V37+V42+V43+V44+V45+PC1, direction = "forward")
+stepAIC(model_null, scope = V50 ~ V1+V2+V3+V4+V6+V7+V8+V11+V12+V13+V14+V15+V16+V17+V19+V20+V21+V22+V23+V27+V28+V29+V24+V30+V31+V32+V33+V34+V35+V36+V37+V38+V40+V41+V42+V43+V44+V45, direction = "forward", data = data)
 
-# Remove V30, V31, V33, V34, V35, V42, V43, V44 from model
-model_updated <- glm(V50 ~ V24 + V32 + V36 + V37 + V45 + PC1,family = binomial(link = "logit"))
-explanatory_var_updated <- c('V24', 'V32', 'V36', 'V37', 'V45','PC1')
+data_Updated<- data[c('V2','V4','V7','V11','V14','V20','V21','V22','V27','V29','V24','V30','V31','V34','V41','V45','V50')]
+model_updated <- glm(V50 ~ V2 + V4 + V7 + V11 + V14 + V20 + V21+V22 +V27+V29+V24+V30+V31+V34+V41+V45,family = binomial(link = "logit"))
+explanatory_var_updated <- c('V2','V4','V7','V11','V14','V20','V21','V22','V27','V29','V24','V30','V31','V34','V41','V45')
 
-res.pca_model_final <- prcomp(data_Updated[c('V24', 'V32', 'V36', 'V37', 'V45','PC1')], scale = TRUE)
+res.pca_model_final <- prcomp(data_Updated[c('V2','V4','V7','V11','V14','V20','V21','V22','V27','V29','V24','V30','V31','V34','V41','V45')], scale = TRUE)
 fviz_eig(res.pca_model_final)
 pdf("circle_pca.pdf")
 fviz_pca_var(res.pca_model_final,
@@ -176,6 +201,7 @@ fitted_values <- model_updated$fitted.values
 summary(fitted_values)
 plt <- ggplot(data.frame(fitted_values), aes(y = fitted_values)) + geom_boxplot()
 plt <- plt + labs(x = "", y = "Fitted values")
+plt
 ggsave("boxplots_fitted_values.pdf", plt)
 
 #----------------------------------------#
@@ -185,6 +211,7 @@ residuals <- resid(model_updated, type = "pearson")
 summary(residuals)
 plt <- ggplot(data.frame(residuals), aes(y = residuals)) + geom_boxplot()
 plt <- plt + labs(x = "", y = "Pearson residuals")
+plt
 ggsave("boxplots_residuals.pdf", plt)
 
 #----------------------------------------#
@@ -196,16 +223,17 @@ LOO_Post<- rep(0, dim(data_Updated)[1])
 for (i in 1:dim(data_Updated)[1]) {
   idx <- rep(TRUE, dim(data_Updated)[1])
   idx[i] <- FALSE
-  linear_classification <- glm(V50[idx] ~ V24[idx] + V32[idx] + V36[idx] + V37[idx] + V45[idx] + PC1[idx], family = binomial(link = "logit"))
+  linear_classification <- glm(V50[idx] ~ V2[idx] +V4[idx] +V7[idx] +V11[idx] +V14[idx] +V20[idx] +V21[idx] +V22[idx] +V27[idx] +V29[idx] +V24[idx] +V30[idx] +V31[idx] +V34[idx] +V41[idx] +V45[idx], family = binomial(link = "logit"))
   prediction <- linear_classification$coefficients %*% c(1, as.matrix(data_Updated[i, explanatory_var_updated]))
   LOO_Post[i] <- exp(prediction)/(1 + exp(prediction))
 }
 
 # Construct confusion matrix
-threshold = 0.5
+threshold = 0.38
 Actual<- V50
 Predicted <- as.integer(LOO_Post> threshold)
 conf_mat <- table(V50,Predicted)
+(40/165)*100 - mean(errors)*100
 
 
 # Compute sensitivity and specificity
@@ -245,7 +273,7 @@ ROC_maker <- function(scores, memberships) {
   
   return(list(spec = (1 - specificity), sens = sensitivity))
 }
-#pdf("ROC.pdf")
+pdf("ROC.pdf")
 ROC <- ROC_maker(LOO_Post, V50)
 abline(a = 0, b = 1, col = "red")
 abline( v = 1-specificity, col = "blue")
